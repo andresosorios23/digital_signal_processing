52abcc0039aba1d2fec568b1afbc022460bac888
andresosorios23 <anosoriosa@unal.edu.co>
2024-02-24 14:11:35 -0500

    primer avance task_1

---
+++ task_1/test.py
@@ -0,0 +1,11 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# %% [markdown]
+# # Manipulacion y representación de señales en Python
+
+# %%
+FS: int = 8000
+t = np.arange(0, 1 / FS, 0.05)
